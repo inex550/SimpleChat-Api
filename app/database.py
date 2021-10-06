2bca2a7826e5a7b7af0771db13ef7a58ab77d72e
iNex <inex552@gmail.com>
2021-10-06 10:53:39 +0300

    save

--- app/database.py
+++ app/database.py
@@ -1,14 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-
-from fastapi import Depends
-
-
-SQLITE_DATABASE_URL = 'sqlite:///./db.db'
+from . import config
 
 engine = create_engine(
-    SQLITE_DATABASE_URL, connect_args={ 'check_same_thread': False }
+    config.POSTGRESQL_DATABASE_URL, #connect_args={ 'check_same_thread': False }
 )
 
 SessionLocal = sessionmaker(engine, autocommit=False, autoflush=False)
